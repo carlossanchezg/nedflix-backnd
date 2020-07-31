7a147e34832d8b2b4dd872c96c2b9bd06e67283d
carlossanchezg <scarlosdaniel@live.com>
2020-07-31 13:26:14 -0600

    separate request to database in its own file/srvce

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,30 +1,29 @@
-const { Users } = require('../models');
+const { UsersService } = require('../services');
 
 module.exports = {
   create: async (req, res) => {
     try {
-      const newUser = new Users(req.body);
-      const saveUser = await newUser.save();
-      res.status(201).json(saveUser);
+      const newUser = await UsersService.create(req.body);
+      res.status(201).json(newUser);
     } catch (error) {
       res.status(400).json(error);
     }
   },
-  getUsers: async (req, res) => {
-    try {
-      const users = await Users.find();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(404).json(error);
-    }
-  },
-  findUserById: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await Users.findById(id);
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(404).json(error);
-    }
-  },
+//   getUsers: async (req, res) => {
+//     try {
+//       const users = await Users.find();
+//       res.status(200).json(users);
+//     } catch (error) {
+//       res.status(404).json(error);
+//     }
+//   },
+//   findUserById: async (req, res) => {
+//     try {
+//       const { id } = req.params;
+//       const user = await Users.findById(id);
+//       res.status(200).json(user);
+//     } catch (error) {
+//       res.status(404).json(error);
+//     }
+//   },
 };
