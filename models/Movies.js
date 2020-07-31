eecb49d474f3bfee7f1a0d8c760ac5e684684a84
carlossanchezg <scarlosdaniel@live.com>
2020-07-31 16:26:07 -0600

    Create movies model

---
+++ models/Movies.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const MoviesSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  gender: {
+    type: [String],
+    required: true,
+    validate: [(value) => value.length > 0, 'The film requires genres'],
+  },
+  synopsis: {
+    type: String,
+  },
+  year: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  cover: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  movie_free: {
+    type: Boolean,
+    required: true,
+    default: Boolean,
+  },
+});
+
+const Movies = mongoose.model('Movies', MoviesSchema);
+
+module.exports = Movies;
