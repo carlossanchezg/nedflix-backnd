55557e0baaa4ba57c27eff39758d24d442da679b
carlossanchezg <scarlosdaniel@live.com>
2020-08-01 16:20:32 -0600

    add user service to add list created in user

--- services/index.js
+++ services/index.js
@@ -1,7 +1,9 @@
 const UsersService = require('./UsersService');
 const MoviesService = require('./MoviesService');
+const UserListsService = require('./UserListsService');
 
 module.exports = {
   UsersService,
   MoviesService,
+  UserListsService,
 };
