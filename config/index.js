20583b5f532aa9a0a36aa43807d79b555b5327f1
carlossanchezg <scarlosdaniel@live.com>
2020-08-04 15:54:38 -0600

    Configuration in mongo uri

--- config/index.js
+++ config/index.js
@@ -2,10 +2,10 @@ const { NODE_ENV } = process.env;
 
 const config = {
   production: {
-    MONGO_URI: process.env.MONGO_URI_PRODUCTION,
+    MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cintanegra-shard-00-00-3w2ae.mongodb.net:27017,cintanegra-shard-00-01-3w2ae.mongodb.net:27017,cintanegra-shard-00-02-3w2ae.mongodb.net:27017/nedflix_production?ssl=true&replicaSet=CintaNegra-shard-0&authSource=admin&retryWrites=true&w=majority`,
   },
   staging: {
-    MONGO_URI: process.env.MONGO_URI_STAGING,
+    MONGO_URI: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cintanegra-shard-00-00-3w2ae.mongodb.net:27017,cintanegra-shard-00-01-3w2ae.mongodb.net:27017,cintanegra-shard-00-02-3w2ae.mongodb.net:27017/nedflix_staging?ssl=true&replicaSet=CintaNegra-shard-0&authSource=admin&retryWrites=true&w=majority`,
   },
 };
 
