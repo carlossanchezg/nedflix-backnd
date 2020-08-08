bc2011bcfa849185bef24dc85dfc9f930fae218d
carlossanchezg <scarlosdaniel@live.com>
2020-08-08 15:18:09 -0600

    Add params in create token

--- utils/index.js
+++ utils/index.js
@@ -10,6 +10,8 @@ module.exports = {
       id: user._id,
       email: user.email,
       first_name: user.first_name,
+      img_profile: user.img_profile,
+      premium: user.premium,
     };
     try {
       const token = jwt.sign(payload, process.env.JWT_SECRET);
