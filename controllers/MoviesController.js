c9b756225c789adca7c65eb0a8db7a60cf7c7039
carlossanchezg <scarlosdaniel@live.com>
2020-08-12 23:24:31 -0600

    res movieBiId in array

--- controllers/MoviesController.js
+++ controllers/MoviesController.js
@@ -22,7 +22,7 @@ module.exports = {
     try {
       const { id } = req.params;
       const movie = await MoviesService.findMovieById(id);
-      res.status(200).json(movie);
+      res.status(200).json([movie]);
     } catch (error) {
       res.status(404).json(error);
     }
